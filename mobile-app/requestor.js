db9b0c4f23ff6135ed94b441330854fe370d0c21
Ray Di Ciaccio <ray.diciaccio@gmail.com>
2011-10-02 03:23:56 -0400

    added some known error codes + messages to mobile-app code

--- mobile-app/requestor.js
+++ mobile-app/requestor.js
@@ -1,5 +1,10 @@
 // Basic AJAX requestor
 
 Pawprint.Requestor = {
-
+	// mapping of known error codes to client-side explanations
+	ERROR_CODES: {
+		"401": "Username and/or password are incorrect",
+		"405": "The specified server does not support RPC",
+		"-32601": "This feature is not supported by your Trac server"
+	}
 };
