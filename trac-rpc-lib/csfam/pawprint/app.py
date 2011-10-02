783ca84e846e44edc5bcd6f515b6d23db6b6e4ae
Ray Di Ciaccio <ray.diciaccio@gmail.com>
2011-10-02 16:54:31 -0400

    fixed module path/import issues when running on the appengine server

--- trac-rpc-lib/csfam/pawprint/app.py
+++ trac-rpc-lib/csfam/pawprint/app.py
@@ -1,6 +1,8 @@
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
-from auth import LoginService
+#from csfam.pawprint.auth import LoginService
+from csfam.pawprint.auth import LoginService
+
 
 application = webapp.WSGIApplication([
     ('/login', LoginService)
